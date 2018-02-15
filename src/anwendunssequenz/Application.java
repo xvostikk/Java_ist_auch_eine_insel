6cfd333cf35e3757b675ec110efceadcac3f4efd
Maryna Khvastova <maryna.khvastova@htw-berlin.de>
2018-02-15 20:58:57 +0100

    Update

--- src/anwendunssequenz/Application.java
+++ src/anwendunssequenz/Application.java
@@ -4,17 +4,25 @@ public class Application {
 	public static void main(String[] args) {
 		System.out.println("Wer morgens total zerknittert aufsteht, ");
 		System.out.println("hat am Tag die besten Entfaltungsmöglichkeiten.");
-		;//Endloswiederholung?
-		System.out.println();//Leere Zeile
+		;// Endloswiederholung?
+		System.out.println();// Leere Zeile
 		;
-		
-		//Overloaded println
-		System.out.println("Verhaften Sie die üblichen Verdächtigen!");//print line=Zeilenvorschubzeichen ausgegeben
+
+		// Overloaded println
+		System.out.println("Verhaften Sie die üblichen Verdächtigen!");// print line=Zeilenvorschubzeichen ausgegeben
 		System.out.println(true);
 		System.out.println(-273);
 		System.out.println();
-		System.out.println(3.14729483049805467587);//ausgibt nicht alle Zahlen!! aufrundet
+		System.out.println(3.14729483049805467587);// ausgibt nicht alle Zahlen!! aufrundet
+
+		// variable Argumentlisten
+		System.out.printf("Was sagst du?%n");// %n = Zeilenumbruch
+		System.out.printf("%d Kanäle und überall nur %s.%n", 220, "Katzen");// %d = Platzhalter für den ganze Zahl, %f=
+																			// float, %s= platzhalter für den
+																			// Zeichenkette(string)
 		
+		
+
 	}
 
 }
