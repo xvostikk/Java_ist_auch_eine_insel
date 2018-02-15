fef8fb0b44307d0dcd19d1f3d6b5573ca3a158dc
Maryna Khvastova <maryna.khvastova@htw-berlin.de>
2018-02-15 20:38:07 +0100

    beginning

---
+++ src/anwendunssequenz/Application.java
@@ -0,0 +1,20 @@
+package anwendunssequenz;
+
+public class Application {
+	public static void main(String[] args) {
+		System.out.println("Wer morgens total zerknittert aufsteht, ");
+		System.out.println("hat am Tag die besten Entfaltungsmöglichkeiten.");
+		;//Endloswiederholung?
+		System.out.println();//Leere Zeile
+		;
+		
+		//Overloaded println
+		System.out.println("Verhaften Sie die üblichen Verdächtigen!");//print line=Zeilenvorschubzeichen ausgegeben
+		System.out.println(true);
+		System.out.println(-273);
+		System.out.println();
+		System.out.println(3.14729483049805467587);//ausgibt nicht alle Zahlen!! aufrundet
+		
+	}
+
+}
